8141ab3e81d9cec057e9e0b511dabc2f6c819aa8
yurii-suprun <iursuprun@gmail.com>
2026-02-11 22:11:18 +0100

    Added InventoryItem class

---
+++ src/main/java/com/suprun/demo/domain/InventoryItem.java
@@ -0,0 +1,24 @@
+package com.suprun.demo.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InventoryItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productName;
+
+    private Integer quantity;
+}
