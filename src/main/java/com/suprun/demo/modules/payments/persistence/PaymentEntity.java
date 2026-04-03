64729cdb4afafb327585d59c400f3f0e3d1beb3e
yurii-suprun <iursuprun@gmail.com>
2026-04-03 16:05:14 +0200

    Add payment entity

---
+++ src/main/java/com/suprun/demo/modules/payments/persistence/PaymentEntity.java
@@ -0,0 +1,44 @@
+package com.suprun.demo.modules.payments.persistence;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "payment")
+public class PaymentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long orderId;
+
+    @Column(nullable = false)
+    private String status;
+
+    protected PaymentEntity() {
+    }
+
+    public PaymentEntity(Long id, Long orderId, String status) {
+        this.id = id;
+        this.orderId = orderId;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
